561a85a653ec88bfa5a2edc728c061938dc15b6d
Aiden Chan <aiden@Aidens-MacBook-Pro.local>
2024-10-15 15:28:51 +0800

    #today finished my models and started working on different parts of views. #next I wasn't able to test my views so I will do that next class, if there are any errors I will fix them in the next class.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+
+class Prompt(Model):
+    statement = StringField()
+    categories = StringField()
+    correct = BooleanField()
+    likes = IntegerField()
+    prompt = StringField()
+
+     def json_response(self):
+        
+        return {
+            'id': self.id,
+            'statement': self.statement,
+            'likes': self.likes,
+            'correct': self.correct,
+            'categories': self.categories,
+            'prompt': self.prompt
+        }
+
+   
--- app/views.py
+++ app/views.py
@@ -3,3 +3,31 @@
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
 
+@route_post(BASE_URL + 'new', args={'statement':str})
+def new_statement(args):
+    new_fortune = Fortune(
+        statement = args['statement'],
+        likes = 0,
+    )
+
+    new_statement.save()
+
+    return {'statement': new_statement.json_response()}
+
+@route_get(BASE_URL + 'all')
+def all_categories(args):
+    categories_list = []
+
+    for categories in Categories.objects.all():
+        categories_list.append(categories.json_response())
+
+    return {'category':categories_list}
+
+    @route_get(BASE_URL + 'all')
+def all_prompts(args):
+    prompts_list = []
+
+    for prompt in Prompt.objects.all():
+        prompt_list.append(prompt.json_response())
+
+    return {'prompt':prompt_list}
